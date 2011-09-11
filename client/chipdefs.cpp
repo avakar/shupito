7031539d865bd256b52f24b8b315bd73482bc599
Martin Vejnár <vejnar.martin@gmail.com>
2011-09-11 15:41:58 +0200

    Fixed bootrst fuse settings for atxmega a4 devices.

--- client/chipdefs.cpp
+++ client/chipdefs.cpp
@@ -22,13 +22,13 @@ const std::string embedded_chipdefs =
 		" bootrst:16 bootsz:17,18 eesave:19 wdton:20 spien:21:0 rstdsbl:22:1 dwen:23:1"
 		" bodlevel:24,25,26 hwbe:27\n"
 
-	"atxmega128a avrx:1e974600 flash=139264:512,eeprom=2048:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:21 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
+	"atxmega128a avrx:1e974600 flash=139264:512,eeprom=2048:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:22 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
 		" bodlevel:40,41,42 eesave:43 bodact:44,45 lb:56,57 blbat:58,59 blba:60,61 blbb:62,63\n"
-	"atxmega64a avrx:1e964600 flash=69632:256,eeprom=2048:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:21 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
+	"atxmega64a avrx:1e964600 flash=69632:256,eeprom=2048:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:22 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
 		" bodlevel:40,41,42 eesave:43 bodact:44,45 lb:56,57 blbat:58,59 blba:60,61 blbb:62,63\n"
-	"atxmega32a avrx:1e954100 flash=36864:256,eeprom=1024:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:21 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
+	"atxmega32a avrx:1e954100 flash=36864:256,eeprom=1024:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:22 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
 		" bodlevel:40,41,42 eesave:43 bodact:44,45 lb:56,57 blbat:58,59 blba:60,61 blbb:62,63\n"
-	"atxmega16a avrx:1e944100 flash=20480:256,eeprom=1024:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:21 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
+	"atxmega16a avrx:1e944100 flash=20480:256,eeprom=1024:32 jtaguid:0,1,2,3,4,5,6,7 wdper:8,9,10,11 wdwper:12,13,14,15 bodpd:16,17 bootrst:22 jtagen:32 wdlock:33 startuptime:34,35 rstdisbl:36:1"
 		" bodlevel:40,41,42 eesave:43 bodact:44,45 lb:56,57 blbat:58,59 blba:60,61 blbb:62,63\n"
 	;
 
