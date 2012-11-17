abcc1319330440fe4eae4f5349158abb24382242
Martin Vejnár <vejnar.martin@gmail.com>
2012-11-17 09:27:35 +0100

    Bootloader patch attempt backup.

--- shupito2/bootloader/patch.py
+++ shupito2/bootloader/patch.py
@@ -26,6 +26,23 @@ data[0x1e] = 0x08
 data[0x20] = 5
 data[0xe0] = 0x15
 
+#data[0x08:0x0a] = [
+#    0xce, 0xc0                 # 8008: rjmp 0x81a6
+#    ]
+
+#data[0x36:0x38] = [
+#    0xb7, 0xc0                 # 8036: rjmp 0x81a6
+#    ]
+
+#data[0x1a6:0x1b4] = [
+#    0x00, 0x91, 0x78, 0x00,    # 81a6: lds r16, 0x0078
+#    0x01, 0xfd,                # 81aa: sbrc r16, 1
+#    0x27, 0xc0,                # 81ac: rjmp 0x81fc
+#    0x03, 0xfd,                # 81ae: sbrc r16, 3
+#    0x25, 0xc0,                # 81b0: rjmp 0x81fc
+#    0x95, 0xcf                 # 81b2: rjmp 0x80de
+#    ]
+
 fout = open(output_file + '.bin', 'wb')
 fout.write(''.join((chr(b) for b in data)))
 fout.close()
