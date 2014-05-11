5065191eee09d04c5c497649ee4610d1e0d734df
Martin Vejnár <vejnar.martin@gmail.com>
2014-05-11 15:48:46 +0200

    shupito23: new front design

---
+++ shupito23/front_design/compose_front.py
@@ -0,0 +1,26 @@
+with open('front_template.svg', 'r') as fin:
+    templ = fin.read()
+
+lines = templ.split('\n')
+outlines = list(lines[:53])
+
+rows = 7
+cols = 7
+xoff = 17
+yoff = 30
+height = 29
+width = 37
+reps = 4
+i = 0
+for x in xrange(cols):
+    for y in xrange(rows):
+        out = templ.replace('translate(0 24)', 'translate(%d %d)' % (x * width + xoff, y * height + yoff))
+        out = out.replace('2014-01', '2014-%02d' % (1+(i // reps)))
+        i += 1
+        lines = out.split('\n')
+        outlines.extend(lines[53:-2])
+
+outlines.extend(lines[-2:])
+
+with open('out.svg', 'w') as fout:
+    fout.write('\n'.join(outlines))
